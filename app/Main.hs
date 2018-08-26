6e78fc4d83e7660614624ec0f02fbae0a22dd928
Gavin Lam <me@gavin.hk>
2018-08-26 12:59:16 +0800

    Added view composing and rendering using blaze-html

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,15 @@
 module Main where
 
 import qualified Alpaca.View as V
+import Text.Blaze.Html (Html)
+import qualified Text.Blaze.Html.Renderer.Text as R
 import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $
   get "/" $ do
     name <- param "name" `rescue` (\_ -> return "haskell")
-    html $ V.hello name
+    renderHtml $ V.hello name
+
+renderHtml :: Html -> ActionM ()
+renderHtml = html . R.renderHtml
--- src/Alpaca/View.hs
+++ src/Alpaca/View.hs
@@ -5,6 +5,13 @@ module Alpaca.View
   ) where
 
 import Data.Text.Lazy (Text)
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
 
-hello :: Text -> Text
-hello s = "<h1>hello " <> s <> "</h1>"
+hello :: Text -> Html
+hello name = docTypeHtml $ do
+  let greeting = toHtml $ "hello " <> name
+  H.head $ do
+    H.title greeting
+  body $ do
+    h1 greeting
