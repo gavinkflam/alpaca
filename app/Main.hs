d39ffd6fe4032f49c3da2b3cfff7dbc04a892ab2
Gavin Lam <me@gavin.hk>
2018-08-30 00:07:11 +0800

    Added dummy article and corresponding endpoint

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,29 @@
 
 module Main where
 
-import qualified Alpaca.View as V
+import Alpaca.Data.Article
+import Alpaca.View.Article (renderArticle)
 import Text.Blaze.Html (Html)
+import Data.Time.Calendar (fromGregorian)
 import qualified Text.Blaze.Html.Renderer.Text as R
 import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $
-  get "/" $ do
-    name <- param "name" `rescue` (\_ -> return "haskell")
-    renderHtml $ V.hello name
+  get "/article/dummy" $ do
+    renderHtml $ renderArticle dummyArticle
 
+-- Articles
+dummyArticle :: Article
+dummyArticle = Article
+  { _title         = "Dummy article"
+  , _slug          = "dummy"
+  , _coverImage    = "N/A"
+  , _sections      = []
+  , _datePublished = fromGregorian 2018 8 29
+  , _dateModified  = fromGregorian 2018 8 29
+  }
+
+-- Utils
 renderHtml :: Html -> ActionM ()
 renderHtml = html . R.renderHtml
