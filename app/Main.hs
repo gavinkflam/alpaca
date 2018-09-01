aa198b01432f43b032de636b27958a56532a9f0d
Gavin Lam <me@gavin.hk>
2018-09-01 13:05:55 +0800

    Added all types of section to dummy article

--- app/Main.hs
+++ app/Main.hs
@@ -50,6 +50,41 @@ In ac tortor eget purus volutpat scelerisque.
 Etiam condimentum lorem vitae mollis consectetur.
 Sed a metus in nibh elementum tincidunt ut at nisl.
 |]
+    , DivisionHeader "Etiam erat velit"
+    , SubdivisionHeader "Tellus id"
+    , Paragraph [here|
+Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.
+Bibendum enim facilisis gravida neque convallis a cras.
+Pharetra et ultrices neque ornare aenean.
+Nec ultrices dui sapien eget mi. Sodales ut etiam sit amet nisl purus in.
+|]
+    , ParagraphHeader "Porta nibh"
+    , Snippet "ruby" [here|
+def query
+  params[:q]
+end
+|]
+    , ParagraphHeader "Ornare lectus"
+    , Snippet "haskell" [here|
+-- Quicksort
+-- https://learnxinyminutes.com/docs/haskell/
+qsort [] = []
+qsort (p:xs) = qsort lesser ++ [p] ++ qsort greater
+    where lesser  = filter (< p) xs
+          greater = filter (>= p) xs
+|]
+    , DivisionHeader "Tortor aliquam nulla"
+    , SubdivisionHeader "Sagittis id"
+    , Console [here|
+% ls -lA | grep 'zsh'
+-rw-r--r--  1 gavin gavin    346 Aug 31 23:50 .zshenv
+-rw-------  1 gavin gavin  38494 Sep  1 12:19 .zsh_history
+-rw-r--r--  1 gavin gavin    591 Aug  5 03:08 .zsh_plugins
+-rw-r--r--  1 gavin gavin   2320 Aug 31 23:50 .zshrc
+-rw-r--r--  1 gavin gavin   4128 Aug  5 03:08 .zsh_theme
+|]
+    , SubdivisionHeader "Urna et pharetra"
+    , Image "https://www.haskell.org/static/img/haskell-logo.svg"
     ]
   , _datePublished = fromGregorian 2018 8 29
   , _dateModified  = fromGregorian 2018 8 29
