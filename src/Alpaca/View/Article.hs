ce1e59cec5986c02302c73c79f8dcee216b3d10a
Gavin Lam <me@gavin.hk>
2018-09-21 22:21:06 +0800

    Replaced view with (^.)

--- src/Alpaca/View/Article.hs
+++ src/Alpaca/View/Article.hs
@@ -8,25 +8,22 @@ module Alpaca.View.Article
 import Alpaca.Data.Article
 import Alpaca.View.Layout (renderWithLayout)
 import Alpaca.View.Section (renderSections)
-import Control.Lens (view)
+import Control.Lens ((^.))
 import Data.Time.Calendar (toGregorian, showGregorian)
 import Data.Text.Lazy (Text)
 import Text.Blaze.Html5 as H hiding (title, map)
 import Text.Blaze.Html5.Attributes as A hiding (title)
 
 renderArticle :: Article -> Html
-renderArticle article = docTypeHtml $ do
-  let t = view title article
-  renderWithLayout t $ section $ do
-    h1 $ toHtml t
-    time $ toHtml $ showGregorian $ view datePublished article
-    renderSections $ view sections article
+renderArticle article = docTypeHtml $
+  renderWithLayout (article ^. title) $ section $ do
+    h1 $ toHtml $ article ^. title
+    time $ toHtml $ showGregorian $ article ^. datePublished
+    renderSections $ article ^. sections
 
 renderArticleList :: [Article] -> Html
 renderArticleList articles = docTypeHtml $
   renderWithLayout "Articles" $ toHtml $ map renderArticleListItem articles
 
 renderArticleListItem :: Article -> Html
-renderArticleListItem article = H.article $ do
-  let t = view title article
-  h1 $ toHtml t
+renderArticleListItem article = H.article $ h1 $ toHtml $ article ^. title
