b62c65cbd72756921b5fcdc261cb7c3eb570a26f
Gavin Lam <me@gavin.hk>
2018-09-21 22:15:24 +0800

    Added renderArticleList for article listing page

--- src/Alpaca/View/Article.hs
+++ src/Alpaca/View/Article.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Alpaca.View.Article
   ( renderArticle
+  , renderArticleList
   ) where
 
 import Alpaca.Data.Article
@@ -8,7 +11,7 @@ import Alpaca.View.Section (renderSections)
 import Control.Lens (view)
 import Data.Time.Calendar (toGregorian, showGregorian)
 import Data.Text.Lazy (Text)
-import Text.Blaze.Html5 as H hiding (title)
+import Text.Blaze.Html5 as H hiding (title, map)
 import Text.Blaze.Html5.Attributes as A hiding (title)
 
 renderArticle :: Article -> Html
@@ -18,3 +21,12 @@ renderArticle article = docTypeHtml $ do
     h1 $ toHtml t
     time $ toHtml $ showGregorian $ view datePublished article
     renderSections $ view sections article
+
+renderArticleList :: [Article] -> Html
+renderArticleList articles = docTypeHtml $
+  renderWithLayout "Articles" $ toHtml $ map renderArticleListItem articles
+
+renderArticleListItem :: Article -> Html
+renderArticleListItem article = H.article $ do
+  let t = view title article
+  h1 $ toHtml t
